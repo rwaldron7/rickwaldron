51db233e61b65831657a56bc8efd63e64cf04d7c
rwaldron7 <rickwaldron7@gmail.com>
2022-05-23 22:08:34 +1000

    Added take away app and bookings

--- Restaurant/script.js
+++ Restaurant/script.js
@@ -2,8 +2,6 @@
 
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
-console.log(buttons)
-
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const offset = button.dataset.carouselButton === "next" ? 1 : -1
@@ -49,32 +47,92 @@ searchIcon.addEventListener('click', () => {
 
 // Script for searching the website
 
-const queryStart = document.getElementById('query');
-const query = document.createElement('span');
-const noResults = document.getElementById('no-results');
-query.setAttribute("class","query");
-const search = window.location.search.split('=')[1].split('+').join(' ').toLowerCase();
-query.innerHTML = search;
-queryStart.append(query);
-const results = document.getElementsByClassName("search-results");
-searchArray = search.split(' ');
+try {
+    const queryStart = document.getElementById('query');
+    const query = document.createElement('span');
+    const noResults = document.getElementById('no-results');
+    query.setAttribute("class","query");
+    const search = window.location.search.split('=')[1].split('+').join(' ').toLowerCase();
+    query.innerHTML = search;
+    queryStart.append(query);
+    const results = document.getElementsByClassName("search-results");
+    searchArray = search.split(' ');
 
-console.log(searchArray);
-
-for (i = 0; i < searchArray.length; i++) {
-    for (j = 0; j < results.length; j++) {
-        if (results[j].innerHTML.toLowerCase().includes(searchArray[i])) {
-            results[j].style.display='block';
-            noResults.style.display='none';
+    for (i = 0; i < searchArray.length; i++) {
+        for (j = 0; j < results.length; j++) {
+            if (results[j].innerHTML.toLowerCase().includes(searchArray[i])) {
+                results[j].style.display='block';
+                noResults.style.display='none';
+            }
         }
     }
-
+}
+catch(err) {
 }
 
+// Shopping cart
 
+const addButtons = document.querySelectorAll(".add");
+const foodName = document.querySelectorAll(".name");
+const foodPrice = document.querySelectorAll(".price");
 
-    
+console.log();
 
+for (i = 0; i < addButtons.length; i++) {
+    const add = addButtons[i];
+    const food = foodName[i].innerHTML;
+    add.addEventListener("click", () => {
+        const howMany = document.createElement('input');
+        howMany.setAttribute("name",food);
+        howMany.setAttribute("type","number");
+        howMany.setAttribute("min","0");
+        howMany.setAttribute("max","9");
+        howMany.setAttribute("value","0");
+        add.after(howMany);
+    }, {once: true});
+}
 
+if (window.location.pathname == '/Cart.html') {
+    const order = new URLSearchParams(window.location.search);
+    const list = document.getElementById("cart");
+    const submit = document.getElementById("submit-order");
+    var totalPrice = 0;
+    for (const [key, value] of order) {
+        localStorage.setItem(`${key}`, `${value}`);
+    }
+    for (i = 0; i < localStorage.length; i++) {
+        const food = localStorage.key(i);
+        const quantity = parseInt(localStorage.getItem(food));
+        for (j = 0; j < foodName.length; j++) {
+            if (food == foodName[j].innerText) {
+                const price = foodPrice[j].innerText;
+                const priceNum = parseFloat(price.replace("$",""));
+                newItem = document.createElement("tr");
+                newItem.innerHTML = (`<th>${food}</th><td>${quantity}</td><td>${price}</td>`);
+                orderItem = document.createElement("input");
+                orderItem.innerHTML = (`<input type="text" id="${food}" name="${food}" value="${quantity}">`);
+                list.append(newItem);
+                submit.append(orderItem);
+                totalPrice = totalPrice + quantity*priceNum;
+            }
+        }
+    }
+    orderPrice = document.createElement("input");
+    orderPrice.innerHTML = (`<input type="text" id="price" name="Total Price" value="$${totalPrice.toFixed(2)}">`);
+    submit.append(orderPrice);
+    total = document.createElement("tr");
+    total.setAttribute("id","total-price");
+    total.innerHTML = (`<th>Total Price</th><td></td><td>$${totalPrice.toFixed(2)}</td>`);
+    list.append(total);
+}
 
+const clear = document.getElementById("clear");
+try {
+    clear.addEventListener('click', () => {
+    localStorage.clear();
+    location.reload();
+    })
+}
+catch {
+}
 
